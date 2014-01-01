35e4d71cd9dcbd710badb9dcf250fa4e93138e2c
b2un0 <branko.wilhelm@gmail.com>
2014-01-01 16:53:23 +0100

    enh: installer check for Joomla >= 2.5.16 enh: JHttpFactory usage (since 2.5.16) enh: cache sharing

--- wow_avatar.php
+++ wow_avatar.php
@@ -9,77 +9,77 @@
 
 defined('_JEXEC') or die;
 
-class plgJCommentsWoW_Avatar extends JPlugin {
+class plgJCommentsWoW_Avatar extends JPlugin
+{
 
-	public function __construct(&$subject, $config){
-		parent::__construct($subject, $config);
-		if(version_compare(JVERSION, 3, '>=')) {
-			$this->params->set('guild', rawurlencode(JString::strtolower($this->params->get('guild'))));
-			$this->params->set('realm', rawurlencode(JString::strtolower($this->params->get('realm'))));
-		}else{
-			$this->params->set('realm', str_replace(array('%20', ' '), '-', $this->params->get('realm')));
-			$this->params->set('guild', str_replace(array('%20', ' '), '%2520', $this->params->get('guild')));
-		}
-		
-		$this->params->set('region', JString::strtolower($this->params->get('region')));
-	}
+    public function __construct(&$subject, $config)
+    {
+        parent::__construct($subject, $config);
 
-	public function onPrepareAvatar(&$comment) {
-		$comments = array();
-		$comments[0] =& $comment;
-		$this->onPrepareAvatars($comments);
-	}
+        $this->params->set('guild', rawurlencode(JString::strtolower($this->params->get('guild'))));
+        $this->params->set('realm', rawurlencode(JString::strtolower($this->params->get('realm'))));
+        $this->params->set('region', JString::strtolower($this->params->get('region')));
+    }
 
-	public function onPrepareAvatars(&$comments) {
-		$members = $this->getMembers();
-		
-		if(!is_array($members)) {
-			JFactory::getApplication()->enqueueMessage('JComments - WOW Avatar: ' . $members, 'error');
-			return;
-		}
-			
-		foreach($comments as &$comment) {
-			$name = $comment->{ $this->params->get('mapping', 'name') };
-			$name = JString::strtolower($name);
-		
-			foreach($members as $member) {
-				if($name == JString::strtolower($member->character->name)) {
-					$url = 'http://' . $this->params->get('region') . '.battle.net/static-render/' . $this->params->get('region') . '/' . $member->character->thumbnail;
-					$comment->avatar = JHtml::_('image', $url, '');
-					break;
-				}
-			}
-		}
-	}
+    public function onPrepareAvatar(&$comment)
+    {
+        $comments = array();
+        $comments[0] =& $comment;
+        $this->onPrepareAvatars($comments);
+    }
 
-	protected function getMembers() {
-		$url = 'http://' . $this->params->get('region') . '.battle.net/api/wow/guild/' . $this->params->get('realm') . '/' . $this->params->get('guild') . '?fields=members';
-		
-		$cache = JFactory::getCache('wow', 'output');
-		$cache->setCaching(1);
-		$cache->setLifeTime($this->params->get('cache_time', 60));
-		 
-		$key = md5($url);
-		 
-		if(!$result = $cache->get($key)) {
-			try {
-				$http = new JHttp(new JRegistry, new JHttpTransportCurl(new JRegistry));
-				$http->setOption('userAgent', 'Joomla! ' . JVERSION . '; JComments Avatar WoW Character; php/' . phpversion());
-		
-				$result = $http->get($url, null, $this->params->get('timeout', 10));
-			}catch(Exception $e) {
-				return $e->getMessage();
-			}
-		
-			$cache->store($result, $key);
-		}
-		
-		if($result->code != 200) {
-			return __CLASS__ . ' HTTP-Status ' . JHtml::_('link', 'http://wikipedia.org/wiki/List_of_HTTP_status_codes#'.$result->code, $result->code, array('target' => '_blank'));
-		}
-		
-		$result->body = json_decode($result->body);
-		
-		return $result->body->members;		 
-	}
+    public function onPrepareAvatars(&$comments)
+    {
+        $members = $this->getMembers();
+
+        if (!is_array($members)) {
+            JFactory::getApplication()->enqueueMessage('JComments - WOW Avatar: ' . $members, 'error');
+            return;
+        }
+
+        foreach ($comments as &$comment) {
+            $name = $comment->{$this->params->get('mapping', 'name')};
+            $name = JString::strtolower($name);
+
+            foreach ($members as $member) {
+                if ($name == JString::strtolower($member->character->name)) {
+                    $url = 'http://' . $this->params->get('region') . '.battle.net/static-render/' . $this->params->get('region') . '/' . $member->character->thumbnail;
+                    $comment->avatar = JHtml::_('image', $url, '');
+                    break;
+                }
+            }
+        }
+    }
+
+    protected function getMembers()
+    {
+        $url = 'http://' . $this->params->get('region') . '.battle.net/api/wow/guild/' . $this->params->get('realm') . '/' . $this->params->get('guild') . '?fields=members,achievements';
+
+        $cache = JFactory::getCache('wow', 'output');
+        $cache->setCaching(1);
+        $cache->setLifeTime($this->params->get('cache_time', 60));
+
+        $key = md5($url);
+
+        if (!$result = $cache->get($key)) {
+            try {
+                $http = JHttpFactory::getHttp();
+                $http->setOption('userAgent', 'Joomla! ' . JVERSION . '; JComments Avatar WoW Character; php/' . phpversion());
+
+                $result = $http->get($url, null, $this->params->get('timeout', 10));
+            } catch (Exception $e) {
+                return $e->getMessage();
+            }
+
+            $cache->store($result, $key);
+        }
+
+        if ($result->code != 200) {
+            return __CLASS__ . ' HTTP-Status ' . JHtml::_('link', 'http://wikipedia.org/wiki/List_of_HTTP_status_codes#' . $result->code, $result->code, array('target' => '_blank'));
+        }
+
+        $result->body = json_decode($result->body);
+
+        return $result->body->members;
+    }
 }
